6f8432e76303b647aa3b906fc7c674c00e5ad7fd
Tushar Mukharji <79782641+ECEtushar@users.noreply.github.com>
2022-01-29 13:09:23 +0530

    Update api.py

--- api.py
+++ api.py
@@ -45,9 +45,9 @@ def getItems():
 
                 itemDetail = {'Item': cart[i]['item'], 'Price': cart[i]['price'] * cart[i]['quantity'], 'Tax': tax}
 
-                cate = {cart[i]['itemCategory']: itemDetail}
+                
 
-                bill['Category'].append(cate)
+                bill['Category'][0][cart[i]['itemCategory']]=itemDetail
                 price=float((cart[i]['price'] * cart[i]['quantity'])+tax)
                 bill['Total'] += price
 
